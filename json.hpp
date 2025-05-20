ff06e38af6a78a236eaed8445d5ce78722cbaf15
Jan Liebich <liebich114@gmail.com>
2025-05-20 15:25:32 +0200

    bone of projects

---
+++ json.hpp
@@ -0,0 +1,2 @@
+// Stahni originalni soubor z: https://github.com/nlohmann/json
+// Soubor json.hpp chybi, vloz ho sem.
---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+lucky_json_program.exe
+echo.
+echo Druecke eine beliebige Taste zum Beenden...
+pause >nul
